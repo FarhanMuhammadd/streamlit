04e83cfdbd52aeecfce9f0361741c3a1a9b1578b
FarhanMuhammadd <99597245+FarhanMuhammadd@users.noreply.github.com>
2022-05-27 16:43:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -93,10 +93,10 @@ with st.container():
             
 with st.container():
     col6,col7 = st.columns(2)
-    with col1:
+    with col6:
         st.text('Complaints Submitted Via')
         st.plotly_chart(fig)
-    with col5:
+    with col7:
         st.text('Complaints by Date')
         st.line_chart(dd1) 
     
